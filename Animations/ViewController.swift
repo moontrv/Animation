a6d119f37927c4a273504b5da67262cc283f4e1e
mn <pesrobot@gmail.com>
2016-05-24 15:50:10 +0300

    initial commit

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var alientImage: UIImageView!
+    
+    var counter = 1
+    var timer = NSTimer()
+    var isAnimating = true
+    
+    @IBAction func updateImage(sender: AnyObject) {
+        if isAnimating == true{
+            timer.invalidate()
+            isAnimating = false
+        }else{
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.3, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+            isAnimating = true
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.3, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+    }
+    
+    func doAnimation(){
+        if counter == 4{
+            counter = 1
+        }else{
+            counter = counter+1
+        }
+    alientImage.image = UIImage(named: "frame\(counter).png")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +46,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidLayoutSubviews() {
+        //alientImage.center = CGPointMake(alientImage.center.x-400, alientImage.center.y)
+        //alientImage.alpha = 0
+        //alientImage.frame = CGRectMake(100, 20, 0, 0)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(1, animations: { ()->Void in
+            //self.alientImage.center = CGPointMake(self.alientImage.center.x+400, self.alientImage.center.y)
+            //self.alientImage.alpha = 1
+            //self.alientImage.frame = CGRectMake(100, 20, 100, 200)
+        })
+    }
 
 }
 
